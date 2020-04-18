63d49fc02d7e5dc6448060f7e8c554eb5357e74b
Stephanie Krass <slkrass@ksu.edu>
2020-04-18 13:10:08 -0500

    Add project files.

---
+++ BoggleGame/Boggle.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace BoggleGame
+{
+    partial class Boggle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.uxStartLittleBog = new System.Windows.Forms.Button();
+            this.uxReset = new System.Windows.Forms.Button();
+            this.uxMinLabel = new System.Windows.Forms.Label();
+            this.uxColonLabel = new System.Windows.Forms.Label();
+            this.uxSecLabel = new System.Windows.Forms.Label();
+            this.uxCountDownTimer = new System.Windows.Forms.Timer(this.components);
+            this.uxDisplay = new System.Windows.Forms.TextBox();
+            this.uxStartBigBog = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // uxStartLittleBog
+            // 
+            this.uxStartLittleBog.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxStartLittleBog.Location = new System.Drawing.Point(12, 12);
+            this.uxStartLittleBog.Name = "uxStartLittleBog";
+            this.uxStartLittleBog.Size = new System.Drawing.Size(172, 50);
+            this.uxStartLittleBog.TabIndex = 0;
+            this.uxStartLittleBog.Text = "Play Little Boggle";
+            this.uxStartLittleBog.UseVisualStyleBackColor = true;
+            this.uxStartLittleBog.Click += new System.EventHandler(this.uxStart_Click);
+            // 
+            // uxReset
+            // 
+            this.uxReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxReset.Location = new System.Drawing.Point(368, 12);
+            this.uxReset.Name = "uxReset";
+            this.uxReset.Size = new System.Drawing.Size(172, 50);
+            this.uxReset.TabIndex = 1;
+            this.uxReset.Text = "Reset";
+            this.uxReset.UseVisualStyleBackColor = true;
+            this.uxReset.Click += new System.EventHandler(this.uxReset_Click);
+            // 
+            // uxMinLabel
+            // 
+            this.uxMinLabel.AutoSize = true;
+            this.uxMinLabel.BackColor = System.Drawing.Color.Transparent;
+            this.uxMinLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxMinLabel.Location = new System.Drawing.Point(577, 12);
+            this.uxMinLabel.Name = "uxMinLabel";
+            this.uxMinLabel.Size = new System.Drawing.Size(51, 55);
+            this.uxMinLabel.TabIndex = 2;
+            this.uxMinLabel.Text = "3";
+            this.uxMinLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // uxColonLabel
+            // 
+            this.uxColonLabel.AutoSize = true;
+            this.uxColonLabel.BackColor = System.Drawing.Color.Transparent;
+            this.uxColonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxColonLabel.Location = new System.Drawing.Point(612, 9);
+            this.uxColonLabel.Name = "uxColonLabel";
+            this.uxColonLabel.Size = new System.Drawing.Size(34, 51);
+            this.uxColonLabel.TabIndex = 3;
+            this.uxColonLabel.Text = ":";
+            // 
+            // uxSecLabel
+            // 
+            this.uxSecLabel.AutoSize = true;
+            this.uxSecLabel.BackColor = System.Drawing.Color.Transparent;
+            this.uxSecLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxSecLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.uxSecLabel.Location = new System.Drawing.Point(631, 12);
+            this.uxSecLabel.Name = "uxSecLabel";
+            this.uxSecLabel.Size = new System.Drawing.Size(78, 55);
+            this.uxSecLabel.TabIndex = 4;
+            this.uxSecLabel.Text = "00";
+            // 
+            // uxCountDownTimer
+            // 
+            this.uxCountDownTimer.Interval = 1000;
+            this.uxCountDownTimer.Tick += new System.EventHandler(this.uxCountDownTimer_Tick);
+            // 
+            // uxDisplay
+            // 
+            this.uxDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxDisplay.Location = new System.Drawing.Point(12, 83);
+            this.uxDisplay.Multiline = true;
+            this.uxDisplay.Name = "uxDisplay";
+            this.uxDisplay.Size = new System.Drawing.Size(768, 546);
+            this.uxDisplay.TabIndex = 5;
+            // 
+            // uxStartBigBog
+            // 
+            this.uxStartBigBog.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxStartBigBog.Location = new System.Drawing.Point(190, 12);
+            this.uxStartBigBog.Name = "uxStartBigBog";
+            this.uxStartBigBog.Size = new System.Drawing.Size(172, 50);
+            this.uxStartBigBog.TabIndex = 6;
+            this.uxStartBigBog.Text = "Play Big Boggle";
+            this.uxStartBigBog.UseVisualStyleBackColor = true;
+            this.uxStartBigBog.Click += new System.EventHandler(this.uxStart_Click);
+            // 
+            // Boggle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 641);
+            this.Controls.Add(this.uxSecLabel);
+            this.Controls.Add(this.uxStartBigBog);
+            this.Controls.Add(this.uxDisplay);
+            this.Controls.Add(this.uxColonLabel);
+            this.Controls.Add(this.uxMinLabel);
+            this.Controls.Add(this.uxReset);
+            this.Controls.Add(this.uxStartLittleBog);
+            this.Name = "Boggle";
+            this.Text = "Boggle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxStartLittleBog;
+        private System.Windows.Forms.Button uxReset;
+        private System.Windows.Forms.Label uxMinLabel;
+        private System.Windows.Forms.Label uxColonLabel;
+        private System.Windows.Forms.Label uxSecLabel;
+        private System.Windows.Forms.Timer uxCountDownTimer;
+        private System.Windows.Forms.TextBox uxDisplay;
+        private System.Windows.Forms.Button uxStartBigBog;
+    }
+}
---
+++ BoggleGame/Boggle.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace BoggleGame
+{
+    /// <summary>
+    /// A GUI for a simple Boggle game
+    /// </summary>
+    public partial class Boggle : Form
+    {
+        /// <summary>
+        /// Holds the number of minutes for the timer
+        /// </summary>
+        private int _minutes = 3;
+
+        /// <summary>
+        /// Holds the number of seconds for the time
+        /// </summary>
+        private int _seconds = 0;
+
+        /// <summary>
+        /// Generates the dice for the boggle game board
+        /// probably should have implemented a different way, but I know this way allows
+        /// for the dice to be reused and for the dice to be only used once per round
+        /// </summary>
+        List<string> diceList = new List<string>();
+
+        /// <summary>
+        /// "Little Boggle" dice 
+        /// </summary>
+        string[] littleBoggle = new string[16]{ "AAEEGN", "ABBJOO", "ACHOPS", "AFFKPS",
+                                             "AOOTTW", "CIMOTU", "DEILRX", "DELRVY", 
+                                             "DISTTY", "EEGHNW", "EEINSU", "EHRTVW", 
+                                             "EIOSST", "ELRTTY", "HIMNQU", "HLNNRZ"};
+
+        /// <summary>
+        /// "Big Boggle" dice 
+        /// </summary>
+        string[] bigBoggle = new string[] { "AAAFRS", "AAEEEE", "AAFIRS", "ADENNN", "AEEEEM", 
+                                            "AEEGMU" , "AEGMNN" , "AFIRSY" , "BJKQXZ" , "CCNSTW",
+                                            "CEIILT" , "CEILPT" , "CEIPST" , "DHHNOT" , "DHHLOR", 
+                                            "DHLNOR" , "DDLNOR" , "EIIITT" , "EMOTTT" , "ENSSSU", 
+                                            "FIPRSY" , "GORRVW" , "HIPRRY" , "NOOTUW" , "OOOTTU"};
+        /// <summary>
+        /// Allows the size of the board to be changed from a 4x4 to a 5x5
+        /// </summary>
+        int SIZE_OF_BOARD;
+
+        /// <summary>
+        /// Initialization of the GUI
+        /// </summary>
+        public Boggle()
+        {
+            InitializeComponent();
+        }
+        
+        /// <summary>
+        /// Handles the Click Event for the Play Little Boggle
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxStart_Click(object sender, EventArgs e)
+        {
+            uxMinLabel.Text = "3";
+            uxSecLabel.Text = "00";
+            _minutes = 3;
+            _seconds = 0;
+            Button button = (Button)sender;
+            if (button.Text.Equals("Play Little Boggle"))
+            {
+                getDice(littleBoggle); //adds all the dice to the list each time to make sure they have only been used once
+                SIZE_OF_BOARD = 4;// 4x4 board
+            }
+            else
+            {
+                getDice(bigBoggle); //adds all the dice to the list each time to make sure they have only been used once
+                SIZE_OF_BOARD = 5;// 5x5 board
+            }
+            makeBoard();//uxDisplay is the large textbox in case there is any confusion
+            
+            uxMinLabel.ForeColor = Color.Black;
+            uxSecLabel.ForeColor = Color.Black;
+            uxColonLabel.ForeColor = Color.Black;
+            uxCountDownTimer.Start();//starts the timer
+        }
+
+        /// <summary>
+        /// Handles the Timer with 1000ms interval and allows for the count
+        /// down to be visibly displayed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxCountDownTimer_Tick(object sender, EventArgs e)
+        {
+            _seconds--;//decrement seconds on each pass of the timer interval
+            if(_seconds == -1)
+            {//allows for seconds to countdown properly and update the minutes
+             //once seconds runout
+                _minutes--;
+                _seconds = 59;
+            }
+            //converting to a string to update the labels
+            string minutes = _minutes.ToString();
+            string seconds = _seconds.ToString();
+
+            if(seconds.Length == 1)
+            {//handles formating so that once seconds are no longer 
+             //double digits there is a leading zero displayed
+                string temp = "0" + seconds;
+                seconds = temp;
+            }
+            
+            if(_minutes == 0 && _seconds <= 10)
+            {
+                uxMinLabel.ForeColor = Color.Red;
+                uxColonLabel.ForeColor = Color.Red;
+                uxSecLabel.ForeColor = Color.Red;
+            }
+            else
+            {
+                uxMinLabel.ForeColor = Color.Black;
+                uxSecLabel.ForeColor = Color.Black;
+                uxColonLabel.ForeColor = Color.Black;
+            }
+            //Updating the labels
+            uxMinLabel.Text = minutes;
+            uxSecLabel.Text = seconds;
+            if (_minutes == 0 && _seconds == 0)
+            {//If the timer is done, stop and play alarm sounds and display a message
+                uxCountDownTimer.Stop();
+                //Like covering the board with the shield when the timer is done
+                string temp = uxDisplay.Text;
+                uxDisplay.Text = "";
+                MessageBox.Show("Times Up!\nPut Down What You Are Writing With!");
+                //"Uncovering the board so everyone can see how we came up with different words
+                uxDisplay.Text = temp;
+            }
+            else if(_minutes == 0 && _seconds == 1)
+            {//used else if bc there is about a 1 sec delay for the sound to play
+                playSound();
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click Event for the Reset Button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxReset_Click(object sender, EventArgs e)
+        {
+            //resets the game to a new start time and stops the timer
+            uxMinLabel.ForeColor = Color.Black;
+            uxSecLabel.ForeColor = Color.Black;
+            uxColonLabel.ForeColor = Color.Black;
+            _minutes = 3;
+            _seconds = 0;
+            uxMinLabel.Text = "3";
+            uxSecLabel.Text = "00";
+            uxCountDownTimer.Stop();
+        }
+
+        /// <summary>
+        /// Plays a sound to notify players that the game is over
+        /// </summary>
+        private void playSound()
+        {//C:\Users\slkra\OneDrive\Desktop\VirtualGames\media.io_endSound.wav
+            SoundPlayer endAlert = new SoundPlayer(@"C:\Users\slkra\OneDrive\Desktop\VirtualGames\Boggle\soundFiles\warning_horn.wav");
+            endAlert.Play();
+        }
+
+        /// <summary>
+        /// Generates the Boggle board and displays it
+        /// </summary>
+        private void makeBoard()
+        {
+            StringBuilder sb = new StringBuilder("\r\n");
+            Random r = new Random();
+            for(int row = 0; row < SIZE_OF_BOARD; row++)
+            {
+                sb.Append("   ");
+                for (int col = 0; col < SIZE_OF_BOARD; col++)
+                {
+                    int index = r.Next(diceList.Count);
+                    string lettersOnDie = diceList[index];
+                    diceList.RemoveAt(index);//removes the die that has been used so it can't be used again
+                    char c = lettersOnDie[r.Next(lettersOnDie.Length)];
+                  
+                    if(c == 'I')
+                    {
+                            sb.Append(" ");
+                            sb.Append(c.ToString());
+                    }
+                    else if(c == 'Q')
+                    {//adds a u to go with Q (I saw it I assume on some newer boggle game boards)
+                        string temp = c.ToString() + "u";  
+                        sb.Append(temp.PadRight(10)); 
+                    }
+                    else
+                    {
+                        sb.Append(c.ToString());
+                    }
+                    
+                    if (col < SIZE_OF_BOARD - 1)
+                    {
+                        sb.Append("\t");
+                    }
+                    
+                    
+                }
+                /*
+                if(row < (SIZE_OF_BOARD - 1))
+                {
+                    sb.Append("\r\n");
+                }
+                */
+               if(row < (SIZE_OF_BOARD - 1) && SIZE_OF_BOARD == 4)
+                {
+                    sb.Append("\r\n\r\n\r\n");
+                }
+                else if (row < (SIZE_OF_BOARD - 1) && SIZE_OF_BOARD == 5)
+                {
+                    sb.Append("\r\n\r\n\r\n");
+                }
+                
+            }
+            uxDisplay.Text = sb.ToString();
+        }
+
+        /// <summary>
+        /// Generates the list of available dice
+        /// </summary>
+        /// <param name="diceArray">The array of dice for the Boggle Game</param>
+        private void getDice(string[] diceArray)
+        {
+            foreach (string die in diceArray)
+                diceList.Add(die);
+        }
+
+    }
+}
